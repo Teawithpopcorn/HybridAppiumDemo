8961f69a5ff1da3961fa00f7d08869e7a5add83c
holy <haoer715@gmail.com>
2017-10-28 14:40:51 +0800

    Build the search bar and recipe list, and now the for every receipt, it's hard code.

--- shopping/src/recipeList/RecipesListPage.js
+++ shopping/src/recipeList/RecipesListPage.js
@@ -1,20 +1,52 @@
 import React, {Component} from 'react';
 import {
-  Page
+  Page,
+  List,
+  ListItem,
+  SearchInput
 } from 'react-onsenui';
+import _ from 'lodash'
 import NavBar from '../shared/NavBar';
+import recipes from './AllRecipes'
 
 export default class RecipeListPage extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      searchText: null
+    }
+  }
+
+  searchTextChanged(text) {
+    this.setState({
+      searchText: text
+    });
+  }
+
+  filterRecipes() {
+    return recipes
+  }
+
+  renderRecipe(row, index) {
+    return (
+      <ListItem key={index}>
+        <div className='left'>
+          <img src={`http://placekitten.com/g/40/40`} className='list-item__thumbnail' />
+        </div>
+        <div className='center'>
+          'name'
+      </div>
+      </ListItem>)
   }
 
   render() {
     const {navigator} = this.props;
     return (
       <Page renderToolbar={() => <NavBar title='附近美食' navigator={navigator} />}>
-        <section style={{margin: '16px', textAlign: 'center'}}>
-        </section>
+        <div style={{padding: '8px 10px', backgroundColor: '#fafafa'}}>
+          <SearchInput placeholder='Search' style={{width: '100%'}} />
+        </div>
+        <List dataSource={this.filterRecipes.bind(this)()} renderRow={this.renderRecipe.bind(this)}/>
       </Page>
     )
   }
