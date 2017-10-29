5ab3f314c9e275684721a244dc086bb91fd431df
holy <haoer715@gmail.com>
2017-10-29 13:19:56 +0800

    Change endpoint to heroku for test.

--- shoppingIOS/shoppingIOS/ViewController.swift
+++ shoppingIOS/shoppingIOS/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController, WKUIDelegate {
         webView.backgroundColor = UIColor(red: 250.0/255, green: 250.0/255, blue: 250.0/255, alpha: 1.0)
         webView.scrollView.backgroundColor = UIColor(red: 250.0/255, green: 250.0/255, blue: 250.0/255, alpha: 1.0)
         
-        let myURL = URL(string: "http://10.10.10.101:3000")
+        let myURL = URL(string: "https://tw-shopping.herokuapp.com")
         let myRequest = URLRequest(url: myURL!)
         webView.uiDelegate = self
         webView.load(myRequest)
