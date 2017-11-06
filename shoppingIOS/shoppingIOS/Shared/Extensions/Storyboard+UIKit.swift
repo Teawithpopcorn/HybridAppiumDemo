f9e2b9d8b6256b9ba11e972673f2e212f7451f8f
holy <haoer715@gmail.com>
2017-11-06 21:24:16 +0800

    Add userName and password icon to field.

--- shoppingIOS/shoppingIOS/Shared/Extensions/Storyboard+UIKit.swift
+++ shoppingIOS/shoppingIOS/Shared/Extensions/Storyboard+UIKit.swift
@@ -114,7 +114,40 @@ class ExtensionImageView: UIImageView {
 
 @IBDesignable
 class ExtensionTextField: UITextField {
-
+    override func leftViewRect(forBounds bounds: CGRect) -> CGRect {
+        var textRect = super.leftViewRect(forBounds: bounds)
+        textRect.origin.x += leftPadding
+        return textRect
+    }
+    
+    @IBInspectable var leftImage: UIImage? {
+        didSet {
+            updateView()
+        }
+    }
+    
+    @IBInspectable var leftPadding: CGFloat = 0
+    
+    @IBInspectable var color: UIColor = UIColor.lightGray {
+        didSet {
+            updateView()
+        }
+    }
+    
+    func updateView() {
+        if let image = leftImage {
+            leftViewMode = UITextFieldViewMode.always
+            let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
+            imageView.image = image
+            imageView.tintColor = color
+            leftView = imageView
+        } else {
+            leftViewMode = UITextFieldViewMode.never
+            leftView = nil
+        }
+        
+        attributedPlaceholder = NSAttributedString(string: placeholder != nil ?  placeholder! : "", attributes:[NSAttributedStringKey.foregroundColor: color])
+    }
 }
 
 @IBDesignable
